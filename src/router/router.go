bbb0d3e57ae8ff125a638de7f81ec42db1528db4
shivanshk <shivansh.kuchchal@dltlabs.io>
2020-11-10 12:47:10 +0530

    Rearrange route order.

--- src/router/router.go
+++ src/router/router.go
@@ -19,16 +19,16 @@ func Handlers() http.Handler {
 	router.Use(middleware.NoraGuard)
 	router.Use(middleware.ResponseHeader)
 
-	router.HandleFunc("/api/noraAccess/time", func(writer http.ResponseWriter, req *http.Request) {
-		writer.WriteHeader(http.StatusOK)
-		_, _ = writer.Write([]byte(fmt.Sprintf(`{"time":%d}`, constants.NoraTime)))
-	}).Methods(http.MethodGet, http.MethodOptions)
-
 	// router.HandleFunc("/api/noraAccess/memory/{memoryID}", ).Methods(http.MethodGet, http.MethodOptions)
 	// router.HandleFunc("/api/noraAccess/memory", ).Methods(http.MethodGet, http.MethodOptions)
 	// router.HandleFunc("/api/noraAccess/memory", ).Methods(http.MethodPost, http.MethodOptions)
 	// router.HandleFunc("/api/noraAccess/memory", ).Methods(http.MethodPatch, http.MethodOptions)
 	// router.HandleFunc("/api/noraAccess/memory", ).Methods(http.MethodDelete, http.MethodOptions)
+
+	router.HandleFunc("/api/noraAccess/time", func(writer http.ResponseWriter, req *http.Request) {
+		writer.WriteHeader(http.StatusOK)
+		_, _ = writer.Write([]byte(fmt.Sprintf(`{"time":%d}`, constants.NoraTime)))
+	}).Methods(http.MethodGet, http.MethodOptions)
 
 	router.HandleFunc("/api", func(writer http.ResponseWriter, req *http.Request) {
 		writer.WriteHeader(http.StatusOK)
